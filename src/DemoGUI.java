872d814d18dbecf9f12e4c09cab167a04fd8b7aa
Jirkos6 <achilles_jiri@oauh.cz>
2023-01-30 08:06:14 +0100

    jaky je mastr

--- src/DemoGUI.java
+++ src/DemoGUI.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class DemoGUI {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+
+public class DemoGUI extends JFrame {
+    private JTextField txtName;
+    private JButton btnClick;
+    private JPanel panelMain;
+    private JTextPane textPane1;
+    private JButton kopirujButton;
+
+
+
+    public DemoGUI() {
+
+
+        textPane1.setEditable(false);
+
+
+        btnClick.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Random generator = new Random();
+                int nahodne = generator.nextInt(100); // -> 0..99
+                System.out.println(nahodne);
+
+             //   textPane1.setText(txtName.getText() + "\n" + nahodne);
+
+
+                txtName.setText(Integer.toString(nahodne));
+
+
+            }
+        });
+
+        kopirujButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e ){
+                textPane1.setText(textPane1.getText() + txtName.getText() + "\n");
+
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        DemoGUI h = new DemoGUI();
+        h.setContentPane(h.panelMain);
+        h.setTitle("java");
+        h.setBounds(600,200,600,200);
+        h.setVisible(true);
+        h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+    }
 }
